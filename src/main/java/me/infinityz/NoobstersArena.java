c7e8ff6194487542f4c1b604a4c6f675ec731f88
InfinityZ25 <henixceo@gmail.com>
2019-08-01 11:58:10 -0400

    Commit

---
+++ src/main/java/me/infinityz/NoobstersArena.java
@@ -0,0 +1,12 @@
+package me.infinityz;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NoobstersArena extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+        Bukkit.broadcastMessage("Disable");
+    }
+}
